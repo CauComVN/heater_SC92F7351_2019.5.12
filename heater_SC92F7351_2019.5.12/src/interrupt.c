7500faf40e3deec67e7d5240d3eafde870d600c3
cai <cai@qq.com>
2019-05-21 08:00:17 +0800

    1.中断集中处理

---
+++ heater_SC92F7351_2019.5.12/src/interrupt.c
@@ -0,0 +1,33 @@
+#include "Function_Init.H"
+
+void ADC_Interrupt(void) interrupt 6
+{
+    ADC_Interrupt_Handle();
+}
+
+void EX0_Interrupt() interrupt	0
+{
+    Leakage_EX0_Handle();
+}
+
+void EX2_Interrupt() interrupt	10
+{
+	if(!(P2CON&0x02) && (P21==1) ){		
+		//INT25 端口P21 上升沿中断 P21=1
+    Water_Detection_EX2_Handle();
+	}
+	if(!(P2CON&0x01) && (P20==1)){	
+	//INT24 端口P20 上升沿中断 P20=1
+    Zero_Crossing_EX2_Handle();
+	}
+}
+
+void Timer0_Interrupt() interrupt 1
+{
+    Water_Detection_Timer0_Handle();
+}
+
+void PWMInt(void) interrupt 8
+{	
+		Scr_Driver_PWMInt_Handle();
+}
